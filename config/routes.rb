b60b27649661416c62eb4d60a0ddc0d6a7df9478
keymarc22 <cloraltkeymar22@gmail.com>
2022-09-26 22:09:07 -0400

    add model and initial logic for line item date

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#index"
 
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
   resources :pages, only: :index
 end
